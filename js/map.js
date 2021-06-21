91e58e2960a4fdab1093415841c5bdea47764cde
rileysweeneyfleming <riley.sweeney@flemingcollege.ca>
2021-06-21 11:00:36 -0400

    revert edit

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-//var walnut = L.layerGroup();
+var walnut = L.layerGroup();
 
 function PopUp(hideOrshow) {
     if (hideOrshow == 'show') document.getElementById('ac-wrapper');
@@ -6,8 +6,8 @@ function PopUp(hideOrshow) {
 }
 var mymap = L.map('mapid', {
     center: [44.6, -78.5],
-    zoom: 8
-    //layers: [walnut]   
+    zoom: 8,
+    layers: [walnut]   
 });
     
 var basemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
